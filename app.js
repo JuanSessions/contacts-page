3ef27ba32684579bcd1c12e62b4d332c57765157
JuanSessions <juanperusin@yahoo.es>
2020-04-22 15:46:59 +0200

    index route n controller

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const express = require("express")
 // import handler http errors
 // const httpError = require("http-errors")
 
+const indexRoute = require("./routes/indexRoute")
+
 // create express-server
 const app = express()
 
 app.use(express.json())
 
+// use routes
+app.use("/", indexRoute)
+
+
 // create port
 const port = process.env.PORT || 3000
-
-
 
 // listen server
 app.listen(port, () => {
---
+++ routes/indexRoute.js
@@ -0,0 +1,8 @@
+const Route = require("express").Router()
+const {
+    indexController
+} = require("../controllers/indexController")
+
+Route.get("/", indexController)
+
+module.exports = Route
