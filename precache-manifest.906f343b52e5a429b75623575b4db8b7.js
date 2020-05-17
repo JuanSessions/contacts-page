e6003fb1f583a43d42ee29aae91c921a8359b1b7
JuanSessions <juanperusin@yahoo.es>
2020-05-17 19:08:31 +0200

    Updates

---
+++ precache-manifest.906f343b52e5a429b75623575b4db8b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1ad09700d6dd919386e158c17ee6a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e25310f5dd6672fbf56",
+    "url": "./static/css/main.f7c6c283.chunk.css"
+  },
+  {
+    "revision": "6d6d8516625270cae040",
+    "url": "./static/js/2.08db8620.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.08db8620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e25310f5dd6672fbf56",
+    "url": "./static/js/main.e1af5aa8.chunk.js"
+  },
+  {
+    "revision": "67cb0986dff4396fe2b5",
+    "url": "./static/js/runtime-main.90f289fb.js"
+  },
+  {
+    "revision": "79a60b0eb038676e60ce019f14ba1268",
+    "url": "./static/media/books.79a60b0e.jpg"
+  }
+]);
